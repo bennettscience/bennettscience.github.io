340fd4b2c65620e1c16475f6ecb0fb69735c13cf
bennettscience <brian.bennett2@gmail.com>
2015-10-15 12:17:01 -0400

    copy text from page to bookmarklet

--- getText/script.js
+++ getText/script.js
@@ -1,14 +1,12 @@
 function getPlainText() {
-  var text = "";
-  if(typeof window.getSelection != "undefined") {
-    text = window.getSelection().toString();
-    console.log(text);
+  // var text = "";
+  // if(typeof window.getSelection != "undefined") {
+  //   text = window.getSelection().toString();
 
     var download = document.getElementById('download');
     download.href = 'data:text/plain;charset:utf-8,' + encodeURIComponent(text);
     var event = new MouseEvent('click');
     download.dispatchEvent(event);
-
   }
   return text;
 }
