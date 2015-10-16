d0bfa63099e5276537e9b5a1b79584860db6eddb
bennettscience <brian.bennett2@gmail.com>
2015-10-15 21:52:26 -0400

    +make page option

--- getText/script.js
+++ getText/script.js
@@ -1,3 +1,14 @@
+function makePage(text) {
+  if(typeof window.getSelection != "undefined") {
+    text = window.getSelection().toString();
+
+    var newPage = window.open("", null, "height=200,width=300,status=yes,menubar=no");
+    newPage.document.write("<html><head><script src='script.js'></script></head>");
+    newPage.document.write("<body><p>" + text + "</p><button id='download' onclick='getPlainText()'>Download</button><a id='download' download='text.txt'></a></body></html>");
+    console.log(text)
+  }
+}
+
 function getPlainText() {
   var text = "";
   if(typeof window.getSelection != "undefined") {
