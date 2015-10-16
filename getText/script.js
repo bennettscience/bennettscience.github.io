9110850de2040269b4d5fb10d928a06a6a1c4843
bennettscience <brian.bennett2@gmail.com>
2015-10-16 07:02:33 -0400

    mvp for demo and bugfix

--- getText/script.js
+++ getText/script.js
@@ -4,17 +4,20 @@ function makePage(text) {
     text = window.getSelection().toString();
 
     var newPage = window.open("", null, "height=200,width=300,status=yes,menubar=no");
-    newPage.document.write("<html><head><script src='script.js'></script></head>");
-    newPage.document.write("<body><p>" + text + "</p><button id='download' onclick='getPlainText()'>Download</button><a id='download' download='text.txt'></a></body></html>");
+    newPage.document.write("<body><a id='download' download='text.txt'></a><p id='copy'>" + text + "</p><button id='download' onclick='getPlainText()'>Download</button><scipt type='text/javascript' src='script.js' defer></script></body></html>");
     console.log(text)
   }
 }
 
-function getPlainText() {
-  var copy = window.getElementById('copy').innerText;
-  var download = document.getElementById('download');
-  download.href = 'data:text/plain;charset:utf-8,' + encodeURIComponent(copy);
-  var event = new MouseEvent('click');
-  download.dispatchEvent(event);
-  return;
+function getPlainText(copy) {
+  var text = "";
+  if(typeof window.getSelection != "undefined") {
+    text = window.getElementById('copy').innerContent;
+
+    var download = document.getElementById('download');
+    download.href = 'data:text/plain;charset:utf-8,' + encodeURIComponent(text);
+    var event = new MouseEvent('click');
+    download.dispatchEvent(event);
+  }
+  return text;
 }
