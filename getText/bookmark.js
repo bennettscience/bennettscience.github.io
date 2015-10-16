4e4b72f06c6bc2d13e4d648b2450ca81427bfcb7
bennettscience <brian.bennett2@gmail.com>
2015-10-16 14:21:46 -0400

    +bookmarklet script for embedding

---
+++ getText/bookmark.js
@@ -0,0 +1,6 @@
+function makeScript() {
+  var script = document.createElement('script');
+  script.type='text/javascript';
+  script.src='https://rawgit.com/bennettscience/bennettscience.github.io/gh-pages/getText/script.js';
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
