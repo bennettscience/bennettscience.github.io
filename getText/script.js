bf05d1c1cd0bff35e1574d458ffe4f2f1270f27b
bennettscience <brian.bennett2@gmail.com>
2015-10-16 11:30:44 -0400

    bugfix

--- getText/script.js
+++ getText/script.js
@@ -4,20 +4,17 @@ function makePage(text) {
     text = window.getSelection().toString();
 
     var newPage = window.open("", null, "height=200,width=300,status=yes,menubar=no");
-    newPage.document.write("<body><a id='download' download='text.txt'></a><p id='copy'>" + text + "</p><button id='download' onclick='getPlainText()'>Download</button><scipt type='text/javascript' src='script.js' defer></script></body></html>");
+    newPage.document.write("<head><script type='text/javascript' src='script.js'></script></head><body><a id='download' download='text.txt'></a><div id='copy'><p>" + text + "</p><button id='download' onclick='getPlainText()'>Download</button></body></html>");
     console.log(text)
   }
 }
 
-function getPlainText(copy) {
-  var text = "";
-  if(typeof window.getSelection != "undefined") {
-    text = window.getElementById('copy').innerContent;
-
+function getPlainText(text) {
+    var text = document.getElementById('copy').innerText;
+    // console.log(text);
     var download = document.getElementById('download');
     download.href = 'data:text/plain;charset:utf-8,' + encodeURIComponent(text);
     var event = new MouseEvent('click');
     download.dispatchEvent(event);
-  }
   return text;
 }
