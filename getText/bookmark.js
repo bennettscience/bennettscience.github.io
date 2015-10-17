b0375c5d35d0c3bedd090a8733969ea1c34103cc
bennettscience <brian.bennett2@gmail.com>
2015-10-17 14:20:01 -0400

    +callback function to run script after install

--- getText/bookmark.js
+++ getText/bookmark.js
@@ -1,6 +1,12 @@
-function makeScript() {
+function makeScript(url, callback) {
   var script = document.createElement('script');
   script.type='text/javascript';
-  script.src='https://rawgit.com/bennettscience/bennettscience.github.io/gh-pages/getText/script.js';
+  script.onload = function() {
+    callback();
+  }
+  script.src = url;
   document.getElementsByTagName('head')[0].appendChild(script);
 }
+makeScript("https://rawgit.com/bennettscience/bennettscience.github.io/gh-pages/getText/script.js", function() {
+  makePage();
+});
