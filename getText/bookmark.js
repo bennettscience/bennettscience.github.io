2f19d6c6636fe4b5f279b7a377bd557a94b19136
bennettscience <brian.bennett2@gmail.com>
2015-10-18 21:39:55 -0400

    +correct link to stylesheet

--- getText/bookmark.js
+++ getText/bookmark.js
@@ -4,7 +4,7 @@ function makePage(text) {
     text = window.getSelection().toString();
 
     var newPage = window.open("", null, "height=200,width=300,status=yes,menubar=no");
-    newPage.document.write("<head><link rel='stylesheet' type='text/css' href='style.css'/><script type='text/javascript' src='https://rawgit.com/bennettscience/bennettscience.github.io/gh-pages/getText/script.js'></script></head><body><a id='download' download='text.txt'></a><div id='copy'><p>" + text + "</p></div><div id='interact'><button id='download' onclick='getPlainText()'>Download</button></div></body></html>");
+    newPage.document.write("<head><link rel='stylesheet' type='text/css' href='http://rawgit.com/bennettscience/bennettscience.github.io/blob/master/getText/style.css'/><script type='text/javascript' src='https://rawgit.com/bennettscience/bennettscience.github.io/gh-pages/getText/script.js'></script></head><body><a id='download' download='text.txt'></a><div id='copy'><p>" + text + "</p></div><div id='interact'><button id='download' onclick='getPlainText()'>Download</button></div></body></html>");
     console.log(text)
   }
 }
