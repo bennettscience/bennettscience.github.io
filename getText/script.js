d3c4dd98cf5da3546c8de7a4505f85c928ff93d8
bennettscience <brian.bennett2@gmail.com>
2015-10-19 14:42:49 -0400

    +styles, landing info

--- getText/script.js
+++ getText/script.js
@@ -1,5 +1,5 @@
 function getPlainText(text) {
-    var text = document.getElementById('copy').innerText;
+    var text = document.getElementById('text').innerText;
     // console.log(text);
     var download = document.getElementById('download');
     download.href = 'data:text/plain;charset:utf-8,' + encodeURIComponent(text);
