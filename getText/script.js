670b916962b5329b3b1946af6803421a26199a69
bennettscience <brian.bennett2@gmail.com>
2015-10-18 21:36:20 -0400

    self-contained script, styles added

--- getText/script.js
+++ getText/script.js
@@ -1,14 +1,3 @@
-function makePage(text) {
-  var text = "";
-  if(typeof window.getSelection != "undefined") {
-    text = window.getSelection().toString();
-
-    var newPage = window.open("", null, "height=200,width=300,status=yes,menubar=no");
-    newPage.document.write("<head><script type='text/javascript' src='script.js'></script></head><body><a id='download' download='text.txt'></a><div id='copy'><p>" + text + "</p><button id='download' onclick='getPlainText()'>Download</button></body></html>");
-    console.log(text)
-  }
-}
-
 function getPlainText(text) {
     var text = document.getElementById('copy').innerText;
     // console.log(text);
