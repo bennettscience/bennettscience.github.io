566656ac40d726a57be45efa1ac79814c1d310be
bennettscience <brian.bennett2@gmail.com>
2015-10-19 21:06:24 -0400

    +landing page styles

--- getText/bookmark.js
+++ getText/bookmark.js
@@ -3,8 +3,8 @@ function makePage(text) {
   if(typeof window.getSelection != "undefined") {
     text = window.getSelection().toString();
 
-    var newPage = window.open("", null, "height=200,width=600,status=yes,menubar=no");
-    newPage.document.write("<head><link rel='stylesheet' type='text/css' href='http://rawgit.com/bennettscience/bennettscience.github.io/master/getText/style.css'/><script type='text/javascript' src='https://rawgit.com/bennettscience/bennettscience.github.io/gh-pages/getText/script.js'></script></head><body><a id='download' download='text.txt'></a><div id='text'><p>" + text + "</p></div><div id='interact'><button id='download' onclick='getPlainText()'>Download</button></div></body></html>");
+    var newPage = window.open("", null, 'width=560,height=360,status=yes,menubar=no');
+    newPage.document.write("<head><link rel='stylesheet' type='text/css' href='http://rawgit.com/bennettscience/bennettscience.github.io/master/getText/style.css'/><script type='text/javascript' src='https://rawgit.com/bennettscience/bennettscience.github.io/gh-pages/getText/script.js'></script></head><body><a id='download' download='text.txt'></a><div id='copy'><p>" + text + "</p></div><div id='interact'><button id='download' onclick='getPlainText()'>Download</button></div></body></html>");
     console.log(text)
   }
 }
