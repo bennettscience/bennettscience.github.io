c322be03204d9bbc60490d245940e9ef53cd46a7
bennettscience <brian.bennett2@gmail.com>
2015-12-09 15:57:31 -0500

    using `escape` to encode ascii from utf-8

--- getText/script.js
+++ getText/script.js
@@ -11,7 +11,7 @@ function getPlainText(text) {
     var text = document.getElementById('copy').innerText;
     // console.log(text);
     var download = document.getElementById('download');
-    download.href = 'data:text/plain;charset:us-ascii,' + unescape(encodeURIComponent(text));
+    download.href = 'data:text/plain;charset:us-ascii,' + escape(encodeURIComponent(text));
     var event = new MouseEvent('click');
     download.dispatchEvent(event);
   return text;
