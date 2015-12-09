ff8422cd095e769e7e3839f23a91e412815d11cf
bennettscience <brian.bennett2@gmail.com>
2015-12-09 15:38:55 -0500

    testing ascii encode

--- getText/script.js
+++ getText/script.js
@@ -1,8 +1,17 @@
+// function encodeText(text) {
+//   var text = document.getElementById('copy').innerText;
+//   var download = document.getElementById('download');
+//   download.href = decodeURIComponent(escape(text));
+//   console.log(text);
+//   return text;
+// }
+
+
 function getPlainText(text) {
     var text = document.getElementById('copy').innerText;
     // console.log(text);
     var download = document.getElementById('download');
-    download.href = 'data:text/plain;charset:utf-8,' + encodeURIComponent(text);
+    download.href = 'data:text/plain;charset:us-ascii,' + encodeURIComponent(text);
     var event = new MouseEvent('click');
     download.dispatchEvent(event);
   return text;
