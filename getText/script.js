d7dc1360118ebe5c1cdb2765fde285035ae20472
bennettscience <brian.bennett2@gmail.com>
2015-12-09 21:22:15 -0500

    testing ascii encoding on dev deploy

--- getText/script.js
+++ getText/script.js
@@ -10,8 +10,19 @@
 function getPlainText(text) {
     var text = document.getElementById('copy').innerText;
     // console.log(text);
+
+    var out = "";
+    for (var i=0;i<text.length;i++) {
+      if(text.charCodeAt(i) < 0x80) {
+        out += text.charAt(i);
+      } else {
+        var u = "" + text.charCodeAt(i).toString(16);
+        out += "\\u" + (u.length === 2 ? "00" + u : u.length === 3 ? "0" + u : u);
+      }
+    }
+
     var download = document.getElementById('download');
-    download.href = 'data:text/plain;charset:us-ascii,' + decodeURIComponent(escape(text));
+    download.href = 'data:text/plain;charset:us-ascii,' + encodeURIComponent(out);
     var event = new MouseEvent('click');
     download.dispatchEvent(event);
   return text;
